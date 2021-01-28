a57dc9b932f7dd316e7df898d41c1e86d20244c7
DonCastillo <don.qcastillo@yahoo.com>
2021-01-28 03:17:48 -0700

    improved guessing game

--- 0011_guessing_game.py
+++ 0011_guessing_game.py
@@ -1,22 +1,20 @@
-answer = 5
-
-print('Please guess a number between 1 and 10: ')
-guess = int(input())
+import random
 
 
-if guess < answer:
-    print('Please guess higher')
-    guess = int(input())
-    if guess == answer:
-        print('Well done, you guessed it')
+highest = 10
+answer = random.randint(1, highest)
+print(answer)   # todo: remove after testing
+
+
+
+
+while True:
+    guess = int(input('Please guess a number between 1 and {}: '.format(highest)))
+    if guess < answer:
+        print('Please guess higher')
+    elif guess > answer:
+        print('Please guess lower')
     else:
-        print('Sorry, you have not guessed correctly')
-elif guess > answer:
-    print('Please guess lower')
-    guess = int(input())
-    if guess == answer:
-        print('Well done, you guessed it')
-    else:
-        print('Sorry, you have not guessed correctly')
-else:
-    print('You got it first time')
+        print('You\'ve guessed it right')
+        break;
+    
--- 0020_continue_break.py
+++ 0020_continue_break.py
@@ -15,3 +15,15 @@ for item in shopping_list:
     if item == 'spam':
         continue
     print('Buy ' + item)
+
+# stop printing when i is more than 0 and a divisible by 11
+for i in range(0, 100, 7):
+    print(i)
+    if (i % 11 == 0) and (i > 0):
+        break
+
+# print all numbers from 0 to 20 that aren't divisble by 3 or 5
+for i in range(1, 21):
+    if (i % 3 == 0) or (i % 5 == 0):
+        continue
+    print(i)
