c139cd28c3c9fe1662b8429deb812fd353b2c30a
DonCastillo <don.qcastillo@yahoo.com>
2021-01-28 16:45:38 -0700

    refactored some codes

--- 0003_escape.py
+++ 0003_escape.py
@@ -1,8 +1,8 @@
-splitSpring = 'This string has been \nsplit over\nseveral\nline'
-print(splitSpring)
+split_string = 'This string has been \nsplit over\nseveral\nline'
+print(split_string)
 
-tabbedString = '1\t2\t3\t4\t5'
-print(tabbedString)
+tabbed_string = '1\t2\t3\t4\t5'
+print(tabbed_string)
 
 print('The pet shop owner said "No, no, \'e\'s uh,...he\'s resting".')
 # or
@@ -15,21 +15,21 @@ print("""The pet shop owner said "No, no, 'e's uh,...he's resting".""")
 # split over
 # several
 # lines
-anotherSplitSpring = """This string has been 
+another_split_string = """This string has been 
 split over 
 several 
 lines"""
 
-print(anotherSplitSpring)
+print(another_split_string)
 
 # escapes the new line
 # This string has been split over several lines
-anotherSplitSpring = """This string has been \
+another_split_string = """This string has been \
 split over \
 several \
 lines"""
 
-print(anotherSplitSpring)
+print(another_split_string)
 
 print("C:\\Users\\donqc\\notes.txt");
 print(r"C:\Users\donqc\notes.txt")  # raw string
