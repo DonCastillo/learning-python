c59397f3b8508ecab0abed875698ae89792839bd
DonCastillo <don.qcastillo@yahoo.com>
2021-04-16 18:39:05 -0600

    learning python module

---
+++ 0071_turtledemo.py
@@ -0,0 +1,9 @@
+from turtle import forward, right, done
+# from turtle import * # import everything from the module
+                       # not a best practice
+import turtle 
+forward(150)
+right(250)
+forward(150)
+turtle.circle(75)
+done()
