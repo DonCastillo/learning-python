2b628ea9394e14e076cd425a0ca948a2075eb671
DonCastillo <don.qcastillo@yahoo.com>
2021-04-16 20:28:22 -0600

    learned webbrowser module

---
+++ 0073_web_browser_module.py
@@ -0,0 +1,6 @@
+import webbrowser
+# opens a web browser directed to the specified from the url parameter
+# webbrowser.open("https://www.python.org")   
+
+# chrome = webbrowser.get(using='google-chrome')
+# chrome.open_new('https://www.google.com')
